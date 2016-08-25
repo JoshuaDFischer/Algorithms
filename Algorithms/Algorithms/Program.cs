84fd043e3fd566b4fac267aaa8876dbcc788e2d8
Joshua Fischer <Joshuadf@ymail.com>
2016-08-25 11:07:28 -0400

    Started testing the fib class and timming it with the stopwatch.

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -13,7 +13,27 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            Fibonacci fib = new Fibonacci();
+            Console.WriteLine("Fibonacci");
+            Console.Write("Please enter a start number: ");
+            string ans = Console.Read().ToString();
+            int inputFib = Convert.ToInt32(ans);
+            Console.WriteLine("Fibonacci with input number: " + inputFib);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            int fibResult = fib.Recursion(inputFib);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("Recusion: " + fibResult + "\tTime spent to perform in milliseconds:" + watch.Elapsed.TotalMilliseconds);
 
+            watch.Reset();
+
+            watch.Start();
+            int test = fib.DynamicPrograming(inputFib);
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.Write("Dynamic Programming: " + test + "\tTime spent to perform in milliseconds:" + watch.Elapsed.TotalMilliseconds);
+
+            Console.Read();
         }
     }
 }
